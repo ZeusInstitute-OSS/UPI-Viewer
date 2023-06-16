d953fed8b397c82761cc8aef3987bdc7b761506a
sounddrill31 <sounddrill31@gmail.com>
2023-06-16 19:12:22 +0530

    hope it works this time?

--- app/src/main/java/com/example/upiapp/HomeFragment.java
+++ app/src/main/java/com/example/upiapp/HomeFragment.java
@@ -1,8 +1,7 @@
 package com.example.upiapp;
 
 public class HomeFragment {
-    public HomeFragment() {
-        super(R.id.HomeFragment);
-
-    }
+  //  public HomeFragment() {
+   //     super(R.id.HomeFragment);
+   // }
 }
