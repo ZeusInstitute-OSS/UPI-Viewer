127307b658f1f887c2254d27c3372f844c8c9c57
sounddrill31 <sounddrill31@gmail.com>
2023-06-19 20:26:13 +0530

    a lot of wasted effort on the back button

--- app/src/main/java/com/example/upiapp/Login.kt
+++ app/src/main/java/com/example/upiapp/Login.kt
@@ -1,15 +1,86 @@
 package com.example.upiapp
 
-import android.os.Bundle
-import androidx.appcompat.app.ActionBar
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.NavUtils
 
+import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.FragmentTransaction
+
+
+//private lateinit var binding: ActivityLoginBinding
 class Login : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
-        //val actionBar: ActionBar? = actionBar
-        setSupportActionBar(findViewById(R.id.toolbar2))
+    fun openMainActivity() {
+        //val intent = Intent(this, MainActivity::class.java)
+        setContentView(R.layout.fragment_first)
+        //startActivity(intent)
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val toolbar = findViewById<View>(R.id.toolbar2) as Toolbar
+        setSupportActionBar(toolbar)
+
+
+        val display = supportActionBar
+        display?.title="Log into Paytm API"
+        display?.setDisplayHomeAsUpEnabled(true)
+
+// Display menu item's title by using a Toast.
+fun onOptionsItemSelected(item: MenuItem): Boolean {
+    return when(item.itemId){
+        android.R.id.home -> {
+            finish()
+            true
+
+        }
+
+            else ->return super.onOptionsItemSelected(item)
+        }
+
+        }
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_login)
+
+        //actionbar
+        val actionbar = supportActionBar
+        //set actionbar title
+        actionbar!!.title = "Log In"
+        //set back button
+        actionbar.setDisplayHomeAsUpEnabled(true)
+        actionbar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        //OnBackPressedDispatcher()
+        return true
+    }
+
+
+
 }
+
+        //val actionBar: ActionBar? = actionBar
+        //setSupportActionBar(findViewById(R.id.toolbar2))
+        //actionBar.InflateMenu(R.menu.menu_main);
+        //NavUtils.navigateUpFromSameTask(MainActivity.this);
+        //val mToolbar = findViewById<View>(R.id.toolbar2) as Toolbar
+        //mToolbar.setNavigationIcon(R.drawable.back_foreground);
+
+       // mToolbar.setNavigationOnClickListener() {
+       //     setContentView(R.layout.activity_main)
+       //     finish();
+       //     super.onCreate(savedInstanceState)
+       //     binding = ActivityLoginBinding.inflate(layoutInflater)
+       //     setContentView(binding.root)
+
+       //     binding.toolbar2.setOnClickListener {
+       //         openMainActivity()
+       //    }
+       // }
+
+
--- app/src/main/java/com/example/upiapp/MainActivity.kt
+++ app/src/main/java/com/example/upiapp/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.upiapp
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
