13892eb7fcdd127f46c83e602a2708e5f7755056
sounddrill31 <sounddrill31@gmail.com>
2023-06-19 16:08:33 +0530

    log in screen with partially implemented back buttons

---
+++ app/src/main/java/com/example/upiapp/Login.kt
@@ -0,0 +1,13 @@
+package com.example.upiapp
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+        val actionBar: ActionBar? = actionBar
+        setSupportActionBar(findViewById(R.id.toolbar2))
+}
--- app/src/main/java/com/example/upiapp/MainActivity.kt
+++ app/src/main/java/com/example/upiapp/MainActivity.kt
@@ -1,16 +1,18 @@
 package com.example.upiapp
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.upiapp.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -42,16 +44,27 @@ class MainActivity : AppCompatActivity() {
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
-
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
+        // Handle item selection
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.log_in -> {
+                setContentView(R.layout.activity_login)
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+
+  //  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+  //      // Handle action bar item clicks here. The action bar will
+  //      // automatically handle clicks on the Home/Up button, so long
+  //      // as you specify a parent activity in AndroidManifest.xml.
+  //      return when (item.itemId) {
+  //         R.id.action_settings -> true
+  //          else -> super.onOptionsItemSelected(item)
+  //      }
+  //  }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
@@ -59,3 +72,4 @@ class MainActivity : AppCompatActivity() {
                 || super.onSupportNavigateUp()
     }
 }
+
