21a4d4642b20fb8588a0197e51a6fb74c21881fe
sounddrill31 <sounddrill31@gmail.com>
2023-06-19 16:46:55 +0530

    fixed a mistake

--- app/src/main/java/com/example/upiapp/Login.kt
+++ app/src/main/java/com/example/upiapp/Login.kt
@@ -11,6 +11,5 @@ class Login : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         //val actionBar: ActionBar? = actionBar
         setSupportActionBar(findViewById(R.id.toolbar2))
-        NavUtils.navigateUpFromSameTask(FirstFragment);
     }
 }
