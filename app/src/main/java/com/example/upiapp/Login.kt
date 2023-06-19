2a5435f2158a9957106923afe480cdab950fc073
sounddrill31 <sounddrill31@gmail.com>
2023-06-19 21:26:42 +0530

    attempt to use chatGPT to generate something

--- app/src/main/java/com/example/upiapp/Login.kt
+++ app/src/main/java/com/example/upiapp/Login.kt
@@ -1,6 +1,7 @@
 package com.example.upiapp
 
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
@@ -9,7 +10,9 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.FragmentTransaction
+import com.example.upiapp.databinding.ActivityLoginBinding
 
+private lateinit var binding: ActivityLoginBinding
 
 //private lateinit var binding: ActivityLoginBinding
 class Login : AppCompatActivity() {
@@ -17,6 +20,19 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         setContentView(R.layout.activity_login)
+        //custom code
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.floatingActionButton.setOnClickListener {
+            openMainActivity()
+        }
+
+    }
+
+    private fun openMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
     }
 
 }
