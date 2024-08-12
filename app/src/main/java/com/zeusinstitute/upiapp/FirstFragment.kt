f09c3e72a1cd8cb4ba249c507f2ce9b0d11a34bf
sounddrill31 <sounddrill31@gmail.com>
2024-08-12 14:18:09 +0530

    Send data back to FirstFragment
    Now if you save UPI ID on the login screen, it shows up on the home page

--- app/src/main/java/com/zeusinstitute/upiapp/FirstFragment.kt
+++ app/src/main/java/com/zeusinstitute/upiapp/FirstFragment.kt
@@ -1,11 +1,13 @@
 package com.zeusinstitute.upiapp
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputEditText
 import com.zeusinstitute.upiapp.databinding.FragmentFirstBinding
 
 /**
@@ -14,27 +16,27 @@ import com.zeusinstitute.upiapp.databinding.FragmentFirstBinding
 class FirstFragment : Fragment() {
 
     private var _binding: FragmentFirstBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-
         _binding = FragmentFirstBinding.inflate(inflater, container, false)
         return binding.root
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val sharedPref = requireActivity().getPreferences(Context.MODE_PRIVATE)
+        val savedData = sharedPref.getString("saved_data", null)
 
+
+        if (savedData != null) {
+            binding.textView2.text = savedData
+        }
     }
-
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
